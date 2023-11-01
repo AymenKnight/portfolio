abfb4fa3a6c50f9e3b6c780c7364255fca64202c
Daouadji Aymen <daouadji506@gmail.com>
2023-11-01 21:40:13 +0100

    💄 Create UserDescription comp

---
+++ src/components/user_description/UserDescription.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import UserDescription from './UserDescription';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'UserDescription',
+  component: UserDescription,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    
+  },
+} satisfies Meta<typeof UserDescription>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+  
+  },
+};
+
---
+++ src/components/user_description/UserDescription.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/no-unescaped-entities */
+'use client';
+
+import { motion } from 'framer-motion';
+
+interface UserDescriptionProps {}
+export default function UserDescription({}: UserDescriptionProps) {
+  return (
+    <motion.p
+      className="text-2xl font-medium !leading-[1.5] sm:text-3xl"
+      initial={{
+        y: 100,
+        opacity: 0,
+      }}
+      animate={{
+        y: 0,
+        opacity: 1,
+      }}
+    >
+      <span className="font-bold"> Hello, I'm Aymen Daouadji</span>, a skilled
+      <span className="font-bold"> software developer </span> with expertise in
+      <span className="font-bold  ">
+        {' '}
+        Next, React, React Native, and NestJS.
+      </span>
+      <br />
+      <span className="italic">
+        I specialize in crafting web, mobile, and desktop applications,
+        delivering innovative and user-centric solutions.
+      </span>
+    </motion.p>
+  );
+}
