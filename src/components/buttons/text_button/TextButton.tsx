83a66a82a023bf81a943caebbae01b1c9209c6a2
Daouadji Aymen <daouadji506@gmail.com>
2023-11-02 21:08:18 +0100

    💄 Update TextButton comp

--- src/components/buttons/text_button/TextButton.tsx
+++ src/components/buttons/text_button/TextButton.tsx
@@ -1,12 +1,27 @@
-import clsx from 'clsx';
+import { cn } from '@/lib/utils';
 
+type DefaultButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 interface TextButtonProps {
-  backgroundColor?: string;
+  className?: string;
+  text?: string;
+  children?: React.ReactNode;
 }
 export default function TextButton({
-  backgroundColor = 'bg-white',
-}: TextButtonProps) {
+  text,
+  className,
+  children,
+  ...otherProps
+}: TextButtonProps & DefaultButtonProps) {
   return (
-    <div className={clsx('w-10 h-10 rounded-full', backgroundColor)}></div>
+    <button
+      className={cn(
+        'w-fit h-fit px-4 py-2 text-white rounded-full flex flex-row items-center justify-center gap-2',
+        className,
+      )}
+      {...otherProps}
+    >
+      {text}
+      {children}
+    </button>
   );
 }
