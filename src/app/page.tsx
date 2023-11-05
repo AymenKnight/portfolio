a1e242ca6d3b049d9569da1ea33364cf0556702f
Daouadji Aymen <daouadji506@gmail.com>
2023-11-05 23:47:08 +0100

    ✅ Update the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import HeaderNav from '../components/header_nav';
 import Intro from '../components/intro';
 import ControlsSection from '../components/controls_section';
+import BorderDivider from '../components/border_divider';
+import AboutMe from '../components/about_me';
+import ProjectsList from '../components/projects_list';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center  ">
+    <main className="flex flex-col items-center justify-center gap-10 px-5  ">
       <HeaderNav />
       <Intro />
       <ControlsSection />
+      <BorderDivider />
+      <AboutMe />
+      <ProjectsList />
     </main>
   );
 }
