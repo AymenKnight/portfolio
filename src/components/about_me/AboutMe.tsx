ea4d3cf595c6a4eeec1a261b6add3b80e9ba952c
Daouadji Aymen <daouadji506@gmail.com>
2023-11-05 23:47:27 +0100

    💄 Create AboutMe comp

---
+++ src/components/about_me/AboutMe.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/no-unescaped-entities */
+'use client';
+import SectionHeading from '../section_heading';
+import { motion } from 'framer-motion';
+
+interface AboutMeProps {}
+export default function AboutMe({}: AboutMeProps) {
+  return (
+    <motion.section
+      className="flex flex-col items-center justify-center gap-5 max-w-[45rem] leading-8 "
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
+      <SectionHeading>About Me</SectionHeading>
+      <p>
+        With a Bachelor's degree in{' '}
+        <span className="font-medium">Computer Science</span> and a Master's in
+        <span className="font-medium"> Network Engineering</span>, I bring a
+        solid educational foundation to my extensive software development
+        experience. I excel in web development with{' '}
+        <span className="font-medium">React, JavaScript, and TypeScript</span>,
+        and I'm skilled in front-end development using{' '}
+        <span className="font-medium">CSS, SCSS, and Tailwind.</span>
+        For mobile apps, I use <span className="font-medium">React Native</span>
+        , and I'm well-versed in desktop app development with{' '}
+        <span className="font-medium">Electron.js, Tauri, and JavaFX. </span>
+        Additionally, I have strong back-end skills with{' '}
+        <span className="font-medium">Nest.js.</span> I'm passionate about
+        learning, eager to tackle new challenges, and excited about expanding my
+        horizons in the ever-evolving tech world.
+      </p>
+    </motion.section>
+  );
+}
