1d4e14af632ef3edde67e6953e1fe72668729ee9
Daouadji Aymen <daouadji506@gmail.com>
2023-11-05 23:46:39 +0100

    ➕ add my projects data

--- lib/data.ts
+++ lib/data.ts
@@ -1,8 +1,12 @@
 import React from 'react';
-import { CgWorkAlt } from 'react-icons/cg';
-import { FaReact } from 'react-icons/fa';
 import { LuGraduationCap } from 'react-icons/lu';
-import doctorIO from '@/public/pictures/doctorIO.jpeg';
+import doctorIO from '@/public/pictures/doctorIo.png';
+import pisri from '@/public/pictures/pisri.png';
+import lotusLauncher from '@/public/pictures/lotusLauncher.png';
+import drJarvis from '@/public/pictures/drJarvis.jpg';
+import commerce from '@/public/pictures/commerce.png';
+import walid from '@/public/pictures/walid.png';
+import realtor from '@/public/pictures/realtor.png';
 
 //TODO change the description with yours
 //TODO change the images with your projects images
@@ -64,17 +68,87 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: 'rmtDev',
-    description:
-      'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
-    tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
-    // imageUrl: rmtdevImg,
+    title: 'Dr-jarvis',
+    description: 'a doctor management app built using Java.',
+    tags: ['Java', 'JAVAFX', 'CSS'],
+    imageUrl: drJarvis,
   },
-] as const;
+  {
+    title: 'Clothes Store',
+    description:
+      'The app showcases various clothing items and provides features such as authentication with Firebase, persistence of chosen clothes by the user, and the ability to checkout using Stripe.',
+    tags: ['React', 'JavaScript', 'CSS', 'SCSS', 'Firebase', 'Stripe'],
+    imageUrl: drJarvis,
+  },
+  {
+    title: 'Pisri',
+    description:
+      'Pisri is a mobile application built using React Native framework. It serves as a convenient online store where users can browse and purchase products. The app also provides features such as checkout and the ability to request a delivery person. Users are required to sign up or log in to access the app functionalities. Additionally, users can check the status of their deliveries from a specific screen.',
+    tags: ['React Native', 'Expo', 'TypeScript', 'JavaScript', 'Firebase'],
+    imageUrl: pisri,
+  },
+  {
+    title: 'DoctorIO',
+    description:
+      'Clinicord is a Medical practice management software/system tend to improve healthcare services. It is a platform that allows doctors to manage their patients, appointments, and medical records. It build for the interest of creating a secure and safe way to store and exchange medical records',
+    tags: [
+      'React',
+      'TypeScript',
+      'CSS',
+      'SCSS',
+      'Electron.js,"Nest.js',
+      'PostgreSQL',
+      'Prisma',
+    ],
+    imageUrl: doctorIO,
+  },
+
+  {
+    title: 'Lotus Launcher',
+    description:
+      'Lotus Launcher is a simple utility app for launching the Clinicord server. It provides a convenient way to launch the server and access its features.',
+    tags: ['React', 'Tauri', 'TypeScript', 'JavaScript', 'Rust'],
+    imageUrl: lotusLauncher,
+  },
+
+  // {
+  //   title: 'MADRASATI',
+  //   description:
+  //     '"MADRASATI: the digital school notebook" of school support to help and assist students in primary school and improve their results.',
+  //   tags: ['Android Studio', 'Java', 'XML', 'Gradle'],
+  //   imageUrl: commerce,
+  // },
+  {
+    title: 'Realtor app api',
+    description:
+      'Crafted a high-performance Realtor App API using NestJS, Prisma, and PostgreSQL, empowering real estate professionals with seamless data access and management.',
+    tags: ['JavaScript', 'TypeScript', 'Nest.js', 'Prisma', 'PostgreSQL'],
+    imageUrl: realtor,
+  },
+  {
+    title: 'E-learning platform',
+    description:
+      'Built a feature-rich E-Learning Platform from the ground up, leveraging technologies like NestJS, Prisma, and PostgreSQL for the backend API. Deployed on AWS EC2 for high performance, and developed an intuitive Admin Dashboard using React, TypeScript, JavaScript, CSS, and SCSS. ensuring a user-friendly experience for students and administrators.',
+    tags: [
+      'React',
+      'JavaScript',
+      'CSS',
+      'SCSS',
+      'TypeScript',
+      'Supabase',
+      'Nest.js',
+      'Prisma',
+      'PostgreSQL',
+      'AWS EC2',
+    ],
+    imageUrl: walid,
+  },
+];
 
 export const skillsData = [
   'HTML',
   'CSS',
+  'SCSS',
   'JavaScript',
   'TypeScript',
   'React',
@@ -84,7 +158,16 @@ export const skillsData = [
   'Tailwind',
   'Prisma',
   'Redux',
-  'Express',
   'PostgreSQL',
   'Framer Motion',
+  'JAVAFX',
+  'Electron.js',
+  'Nest.js',
+  'Prisma',
+  'PostgreSQL',
+  'AWS EC2',
+  'React Native',
+  'Tauri',
+  'Java',
+  'Supabase',
 ] as const;
