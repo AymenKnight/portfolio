c347859e60c3f8e2ed159b71603e4f424f224633
Daouadji Aymen <daouadji506@gmail.com>
2023-11-24 22:22:40 +0100

    ✅ Update server action

--- services/server_actions/actions.ts
+++ services/server_actions/actions.ts
@@ -5,20 +5,18 @@ import { Resend } from 'resend';
 export const sendEmail = async (formData: FormData) => {
   const email = formData.get('email') as string;
   const message = formData.get('message') as string;
-  console.log('running in the server', email, message);
 
   const resend = new Resend(process.env.RESEND_API_KEY);
 
-  try {
-    await resend.emails.send({
-      from: 'onboarding@resend.dev',
+  const { data, error } = await resend.emails.send({
+    from: 'onboarding@resend.dev',
 
-      to: 'daouadji505@gmail.com',
-      subject: `New Message From ${email}`,
-      text: `${message}`,
-      reply_to: email,
-    });
-  } catch (error) {
+    to: 'daouadji505@gmail.com',
+    subject: `New Message From ${email}`,
+    text: `${message}`,
+    reply_to: email,
+  });
+  if (error) {
     if (error instanceof Error) {
       return {
         error: error.message,
@@ -32,4 +30,8 @@ export const sendEmail = async (formData: FormData) => {
       error: 'Something went wrong',
     };
   }
+
+  return {
+    data,
+  };
 };
