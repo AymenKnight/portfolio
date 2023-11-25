107ee82d66bd0bf2ce8515459ee0ba2515d3c1b7
Daouadji Aymen <daouadji506@gmail.com>
2023-11-25 02:18:15 +0100

    🎨 add the dark mode for the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth " suppressHydrationWarning={true}>
       <body
-        className={`${inter.className} bg-white text-gray-950 relative pt-28 sm:pt-36 `}
+        className={`${inter.className} bg-white text-gray-950 relative pt-28 sm:pt-36
+        dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 transition
+        `}
       >
         {children}
       </body>
