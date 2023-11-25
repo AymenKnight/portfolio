ebfb8cb542788ecacb9aeaf0695ab7c5fcd7faae
Daouadji Aymen <daouadji506@gmail.com>
2023-11-25 22:35:44 +0100

    🎨 add dark mode on Timeline element

--- src/components/timeline_element/TimelineElement.tsx
+++ src/components/timeline_element/TimelineElement.tsx
@@ -1,6 +1,7 @@
 import { useInView } from 'react-intersection-observer';
 import { VerticalTimelineElement } from 'react-vertical-timeline-component';
 import './style/index.css';
+import { useModeStore } from '@/services/stores/modeStore';
 
 interface TimelineElementProps {
   experience: {
@@ -17,6 +18,7 @@ export default function TimelineElement({
   index,
 }: TimelineElementProps) {
   const { ref, inView } = useInView({});
+  const { mode, setMode } = useModeStore((state) => state);
   return (
     <div ref={ref} key={index} className="vertical-timeline-element">
       <VerticalTimelineElement
@@ -25,23 +27,31 @@ export default function TimelineElement({
         date={experience.date}
         className="container-style "
         contentStyle={{
-          background: '#f3f4f6',
+          background: mode == 'light' ? '#f3f4f6' : 'rgb(255, 255, 255,0.05)',
           boxShadow: 'none',
           border: '1px solid rgb(0, 0, 0, 0.05)',
           textAlign: 'left',
           padding: '1.3rem 1rem',
         }}
         contentArrowStyle={{
-          borderRight: '7px solid  rgb(33, 150, 243)',
+          borderRight:
+            mode === 'dark'
+              ? '7px solid   rgb(255, 255, 255,0.6)'
+              : '7px solid  rgb(33, 150, 243)',
         }}
         iconStyle={{
-          background: 'white',
+          background: mode === 'dark' ? 'rgb(33, 150, 243)' : 'white',
           fontSize: '1.5rem',
+          border: '1px solid rgb(0, 0, 0, 0.05)',
         }}
       >
-        <h3 className="font-semibold capitalize">{experience.title}</h3>
-        <p className="font-normal text-gray-500 !mt-1">{experience.location}</p>
-        <p className="font-normal  text-gray-500 !mt-1">
+        <h3 className="font-semibold capitalize dark:text-white">
+          {experience.title}
+        </h3>
+        <p className="font-normal text-gray-500 dark:text-white/75 !mt-1">
+          {experience.location}
+        </p>
+        <p className="font-normal  text-gray-500 dark:text-white/75 !mt-1">
           {experience.description}
         </p>
       </VerticalTimelineElement>
